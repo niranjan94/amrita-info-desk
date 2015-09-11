6f665535bbfbba7a984a1e29985d635bb01ca068
Niranjan Rajendran <niranjan94@yahoo.com>
2015-09-11 17:28:38 +0530

    Added debug activity

---
+++ app/src/main/java/com/njlabs/amrita/aid/aums/AumsDebugActivity.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015. Niranjan Rajendran <niranjan94@yahoo.com>
+ */
+
+package com.njlabs.amrita.aid.aums;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.njlabs.amrita.aid.BaseActivity;
+import com.njlabs.amrita.aid.R;
+import com.njlabs.amrita.aid.util.ark.logging.Ln;
+
+public class AumsDebugActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle extras = getIntent().getExtras();
+        String response = null;
+        if (extras != null) {
+            response = extras.getString("response");
+        }
+
+        setupLayout(R.layout.activity_aums_data, Color.parseColor("#e91e63"));
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadData(response,"text/html","UTF-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+    }
+}
