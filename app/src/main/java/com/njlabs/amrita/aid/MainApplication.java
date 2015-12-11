a258bcd7a443f391f39701c6b7f87b1d79644ba1
Niranjan Rajendran <niranjan94@yahoo.com>
2015-12-11 20:22:47 +0530

    parse local datastore

--- app/src/main/java/com/njlabs/amrita/aid/MainApplication.java
+++ app/src/main/java/com/njlabs/amrita/aid/MainApplication.java
@@ -13,10 +13,12 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 public class MainApplication extends SugarApp {
 
     public static String key = "bdc0fabcbfa45a3506d1e66a6ff77596";
-    public void onCreate() 
-    {
+    public void onCreate() {
         super.onCreate();
+
         Fabric.with(this, new Crashlytics());
+
+        Parse.enableLocalDatastore(this);
         Parse.initialize(this, "rh6SYwa5Gfxk9rBzIEZvXSloGRl50pMnockRYK5E", "DwA9WHCbzgXBfLMosxl32LPEhZGtEqe2jYVuXhCj");
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
