60e2847666a4b7127f7dd8db0c79297b16fb1b59
Niranjan Rajendran <niranjan94@yahoo.com>
2016-09-25 16:13:58 +0530

    Added base notifications

--- app/src/main/java/com/njlabs/amrita/aid/news/NewsUpdateService.java
+++ app/src/main/java/com/njlabs/amrita/aid/news/NewsUpdateService.java
@@ -9,13 +9,10 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 
@@ -41,10 +38,12 @@ public class NewsUpdateService extends IntentService {
     Context mContext;
     boolean allowNotification = true;
 
+    @SuppressWarnings("unused")
     public NewsUpdateService(String name) {
         super(name);
     }
 
+    @SuppressWarnings("unused")
     public NewsUpdateService() {
         super("NewsUpdateService");
     }
